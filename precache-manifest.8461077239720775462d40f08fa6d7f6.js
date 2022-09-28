04b4612d4956b211ff492f2aa5edbd801402c052
Valentine <ucval@yahoo.com>
2022-09-28 21:17:27 +0200

    Updates

---
+++ precache-manifest.8461077239720775462d40f08fa6d7f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ca8153e6d43ef879606bf8462db1b0",
+    "url": "/Documentry-page/index.html"
+  },
+  {
+    "revision": "170db3fe8671e049ffc9",
+    "url": "/Documentry-page/static/css/main.471a92b9.chunk.css"
+  },
+  {
+    "revision": "0ff45e05dc0cd40b8a10",
+    "url": "/Documentry-page/static/js/2.6df6ae88.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Documentry-page/static/js/2.6df6ae88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170db3fe8671e049ffc9",
+    "url": "/Documentry-page/static/js/main.ab3dbcf1.chunk.js"
+  },
+  {
+    "revision": "f3699949c41b67548b0c",
+    "url": "/Documentry-page/static/js/runtime-main.83b99e10.js"
+  },
+  {
+    "revision": "ac825f6c7f337cf8b45bee182d05606c",
+    "url": "/Documentry-page/static/media/DemoAnimation.ac825f6c.gif"
+  }
+]);
